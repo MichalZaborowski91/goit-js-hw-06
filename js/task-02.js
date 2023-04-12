c92d8f0fdaa749c432f5ea42f67f14b794c59ba0
MichalZaborowski91 <mkzaborowski@gmail.com>
2023-04-12 22:59:06 +0100

    task-02 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+for (let i = 0; i < ingredients.length; i++) {
+  const list = document.createElement('li');
+  list.textContent = ingredients[i];
+  list.classList.add('item');
+  const ingredientsUl = document.querySelector('ul');
+  ingredientsUl.after(list);
+}
