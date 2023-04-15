565c438509ab2a4e9b71dffd9341f4065621cdde
MichalZaborowski91 <mkzaborowski@gmail.com>
2023-04-15 21:27:05 +0100

    task-04 done

---
+++ js/task-04.js
@@ -0,0 +1,16 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-plusplus */
+'use strict';
+const value = document.querySelector('#value');
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+
+let counterValue = 0;
+const clickDecrement = () => {
+  value.innerHTML = counterValue--;
+};
+const clickIncrement = () => {
+  value.innerHTML = counterValue++;
+};
+decrementBtn.addEventListener('click', clickDecrement);
+incrementBtn.addEventListener('click', clickIncrement);
