f2d7b8acedb43e2105dc9f788bc3aabec010bf12
MichalZaborowski91 <mkzaborowski@gmail.com>
2023-04-16 17:07:47 +0100

    task-08 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+'use strict';
 
+const form = document.querySelector('.login-form');
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
+form.addEventListener('submit', handleSubmit);
